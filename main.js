7723f7cd880753a9aa613d3868cd366d4750e06e
tg0xff <tg0xff@proton.me>
2024-05-22 18:25:53 -0600

    Define Gameboard.resetBoard() method

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const Gameboard = (function () {
-  let gameboard = [];
-  return { gameboard };
-})()
+  let board = ["", "", "", "", "", "", "", "", ""];
+  const resetBoard = () => board.map((cell) => (cell = ""));
+  return { board, resetBoard };
+})();
 
 function Player(x_mark) {
   const mark = x_mark ? "X" : "O";
