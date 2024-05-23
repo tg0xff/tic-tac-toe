29675e01c47884b8b297710acb0276986038b3c3
tg0xff <tg0xff@proton.me>
2024-05-22 19:24:23 -0600

    Define checkWinner() method

--- main.js
+++ main.js
@@ -6,6 +6,21 @@ const Gameboard = (function () {
       board[index] = mark;
     }
   };
+  const checkWinner = (mark) => {
+    if (
+      (board[0] === mark && board[1] === mark && board[2] === mark) ||
+      (board[3] === mark && board[4] === mark && board[5] === mark) ||
+      (board[6] === mark && board[7] === mark && board[8] === mark) ||
+      (board[0] === mark && board[4] === mark && board[8] === mark) ||
+      (board[2] === mark && board[4] === mark && board[6] === mark)
+    ) {
+      return 1;
+    }
+    if (!board.includes("")) {
+      return -1;
+    }
+    return 0;
+  };
   return { resetBoard, putMark };
 })();
 
